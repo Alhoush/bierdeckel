d4ae3ef997cf947802528a035df557d108b47942
Alhoush <a.alhoush@outlook.de>
2026-03-29 19:04:11 +0200

    frontend Api URL auf Render setzen

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'http://127.0.0.1:8000'
+    baseURL: window.location.hostname === 'localhost'
+        ? 'http://127.0.0.1:8000'
+        : 'https://bierdeckel.onrender.com'
 });
 
 export default API;
